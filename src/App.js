c5474bd26aa25396f8ab3af2b82c0f3b706c57f7
MrSR2000 <94947959+MrSR2000@users.noreply.github.com>
2023-01-03 16:04:38 +0545

    burget-menu

--- src/App.js
+++ src/App.js
@@ -11,64 +11,88 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Route, Routes } from "react-router";
 import { Link, NavLink } from "react-router-dom";
-import Billing  from "./components/billing";
+import Billing from "./components/billing";
+import { AiOutlineMenu } from "react-icons/ai";
+import { BsFillBagFill, BsFillPenFill } from "react-icons/bs";
+import { CiStopwatch, CiMoneyBill } from "react-icons/ci";
+import { BiMenu } from "react-icons/bi";
+
+// const LinksContainer = style.div`
+// display: flex;
+// flex-direction: column;
+// width: 200px;
+// min-height: 100vh;
+// `;
 
 function App() {
   const [selectedList, setSelectedList] = useState("");
+  const [expanded, setExpanded] = useState(true);
+
   const LISTS = [
     {
       name: "productsList",
       title: "Products List",
       path: "/productsList",
       component: <ProductsList />,
+      icon: <BsFillBagFill />,
     },
     {
       name: "homework",
       title: "homework List",
       path: "/homework",
       component: <Homework />,
+      icon: <BsFillPenFill />,
     },
     {
       name: "counter",
       title: "counter ",
       path: "/counter",
       component: <Counter />,
+      icon: <CiStopwatch />,
     },
     {
       name: "billing",
       title: "billings",
       path: "/billinggg",
-      component: <Billing  />
-    }
+      component: <Billing />,
+      icon: <CiMoneyBill />,
+    },
   ];
   return (
     <div className="App">
-      <div className="links">
-        {LISTS.map((l) => (
-          <NavLink
-            key={l.name}
-            //  onClick={() => setSelectedList(l.name) }
-            to={l.path}
-            className="link"
-          >
-            {l.title}
-          </NavLink>
-        ))}
-      </div>
-      {/* {LISTS.map((l) => selectedList === l.name && l.component )} */}
-      {/* <button onClick={() => setSelectedList("homework")} className = {selectedList == "homework" ? "selected" : ""}>Homework</button>
+      <div className="container">
+        <div className={`links ${expanded ? '' : 'not-expanded'}`}>
+        <div className="burger-menu" onClick={(e) => setExpanded(!expanded)}>
+          <BiMenu size={32} />
+        </div>
+        
+          {LISTS.map((l) => (
+            <NavLink
+              key={l.name}
+              //  onClick={() => setSelectedList(l.name) }
+              to={l.path}
+              className="link"
+            >
+              {l.icon}
+              {expanded && <span>{l.title}</span>}
+            </NavLink>
+          ))}
+        </div>
+        {/* {LISTS.map((l) => selectedList === l.name && l.component )} */}
+        {/* <button onClick={() => setSelectedList("homework")} className = {selectedList == "homework" ? "selected" : ""}>Homework</button>
       <button onClick={() => setSelectedList("counter")} className = {selectedList == "counter" ? "selected" : ""}>Counter</button> */}
-      {/* {selectedList === "homework" && <Homework />}
+        {/* {selectedList === "homework" && <Homework />}
       {selectedList === "counter" && <Counter />} */}
 
-      <Routes>
-        <Route path="/" element={<h1>Welcome</h1>} />
-        <Route path="productsList" element={<ProductsList />} />
-        <Route path="homework" element={<Homework />} />
-        <Route path="counter" element={<Counter />} />
-        <Route path="billinggg" element={<Billing />} />
-      </Routes>
-      <ToastContainer />
+        <Routes>
+          <Route path="/" element={<h1>Welcome</h1>} />
+          <Route path="productsList" element={<ProductsList />} />
+          <Route path="homework" element={<Homework />} />
+          <Route path="counter" element={<Counter />} />
+          <Route path="billinggg" element={<Billing />} />
+        </Routes>
+        <ToastContainer />
+      </div>
     </div>
   );
 }
